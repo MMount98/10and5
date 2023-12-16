062b4dac4649edd40d04ea1a456c1a573d845725
Michael Mount <mount.photos@gmail.com>
2023-12-15 17:44:40 -0700

    fixed an formating issue on buttons

--- src/components/pages/splash.js
+++ src/components/pages/splash.js
@@ -9,7 +9,7 @@ export default function splash() {
     <h1>but the lasting impressions <br></br>that we make. </h1>
   </div>
 </div>
-<div className="flex justify-center space-x-44 mt-44">
+<div className="flex justify-center space-x-44 mt-28">
     <h1 className="border border-#242424 px-24 py-4 font-bricolage">BRAND IDENTITY</h1>
     <h1 className="border border-#242424 px-24 py-4 font-bricolage">CONTENT STRATEGY</h1>
     <h1 className="border border-#242424
