803a7579e77d175c3fe4b23f5e025b7553739289
Michael Mount <mount.photos@gmail.com>
2023-12-17 13:18:28 -0700

    reworked Navbar to have tighter margins and include a contact us button to mailto: clients preferred email

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -3,14 +3,14 @@ import lightLogo from "./images/10_5logo_white.png";
 
 export default function NavBar() {
   return (
-    <nav className="my-16 px-12 py-2 flex justify-between items-center">
+    <nav className="my-16 mx-24 py-2 flex justify-between items-center">
       {/* Logo and home link */}
       <a href="/" className="flex items-center">
         <img src={lightLogo} alt="Light Logo" className="h-16 w-auto" />
       </a>
       
       <div>
-        {/* Navigation Links */}
+        <a href="mailto:rrm@strokeofgeniusco.com" className='contact font-oswald text-xl'>CONTACT US</a>
       </div>
     </nav>
   );
