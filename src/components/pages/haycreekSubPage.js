caa8cd45dd54b63cd626314bfb43dcee459ea72d
Michael Mount <mount.photos@gmail.com>
2024-02-13 12:52:54 -0700

    Built a video overlay carousel to be used on any page need

--- src/components/pages/haycreekSubPage.js
+++ src/components/pages/haycreekSubPage.js
@@ -1,4 +1,11 @@
+//images
 import heroImg from "../images/hayCreek/Main Images/heroImg.jpg";
+import bannerImg from "../images/hayCreek/Main Images/Collage_WhoWeAre_2.jpg";
+
+//videos
+import video1 from "../videos/haycreek/IG_Story 1.mp4"
+import video2 from "../videos/haycreek/IG_Story 2 .mp4"
+import video3 from "../videos/haycreek/IG_Story 3.mp4"
 
 export default function HaycreekSubPage() {
   return (
@@ -31,13 +38,35 @@ export default function HaycreekSubPage() {
           </h3>
           <div className="grid grid-cols-1 md:grid-cols-2 text-center text-xs md:text-sm p-5 gap-y-3 -mt-4">
             <p className="font-napzer">
-              <span className="font-span text-lg">$1,059,233</span> Total Revenue Booked
+              <span className="font-span text-lg">$1,059,233</span> Total
+              Revenue Booked
             </p>
             <p className="font-napzer">
-              +<span className="font-span text-lg">78.8%</span> YoY Total Revenue
+              +<span className="font-span text-lg">78.8%</span> YoY Total
+              Revenue
             </p>
-          
           </div>
+        </div>
+      </div>
+      <div className="bannerImg md:-mt-0 -mt-10">
+        <img
+          src={bannerImg}
+          alt="a collage of images used in the black friday campagin for haycreek hotels"
+        />
+      </div>
+      <div className="flex flex-col items-center mx-4 md:m-12">
+        <div>
+          <h3 className="text-4xl md:text-8xl md:absolute md:left-36 md:ml-2 md:mt-6 md:mb-0 2xl:ml-72 mr-44 mt-12 mb-6 font-span">
+            THE BIGGEST
+          </h3>
+        </div>
+        <div>
+          <h3 className="text-5xl md:text-9xl font-spanlight italic ml-36 md:mt-12 md:ml-96 -mt-8">
+            STAYVINGS
+          </h3>
+        </div>
+        <div>
+          <h3 className="text-6xl md:text-13xl font-span">OF THE YEAR.</h3>
         </div>
       </div>
     </div>
