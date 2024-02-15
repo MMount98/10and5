1b0b2960783c0d3a509af88e4be9c82d215aa103
Michael Mount <mount.photos@gmail.com>
2024-02-15 00:19:21 -0700

    add programming and events section to castle subpage

--- src/components/pages/castleSubPage.js
+++ src/components/pages/castleSubPage.js
@@ -2,11 +2,19 @@
 import heroImage from "../images/castleHotel/Hero Image.jpg";
 import secondImage from "../images/castleHotel/2nd Photo.png";
 import thridImage from "../images/castleHotel/3rd Photo.jpg";
+import fourthImage from "../images/castleHotel/4th Photo.jpg";
+import fifthImage from "../images/castleHotel/5th Photo.jpg";
+
+//Magazine Images
 import magazine1 from "../images/castleHotel/Magazine Photo 1.jpg";
 import magazine2 from "../images/castleHotel/Magazine Photo 2.jpg";
 
+//Videos
+import webPageWalkThrough from "../videos/castle/Castle Hotel and Spa- Website Screen Recording.mp4";
+
 //Components
 import TextScroll from "../textScroll";
+import VideoOverlay from "../videoOverlay";
 
 export default function CastleSubPage() {
   return (
@@ -105,19 +113,65 @@ export default function CastleSubPage() {
           </h3>
         </div>
       </div>
-      <div className="magzinePages flex flex-col md:grid md:grid-cols-2 md:my-36 my-14">
+      <div className="magzinePages flex flex-col md:grid md:grid-cols-2 md:mt-36 mt-14">
         <div>
           <h4 className="font-napzer text-center -mb-7 md:-mb-7">
             WESTCHESTER & HUDSON VALLEY MAGAZINE
           </h4>
-          <img src={magazine1} alt="Magazine page highlting the wedding venue at Castle Hotel and spa" className="p-10" />
+          <img
+            src={magazine1}
+            alt="Magazine page highlting the wedding venue at Castle Hotel and spa"
+            className="p-10"
+          />
         </div>
         <div>
           <h4 className="font-napzer text-center -mb-7 md:-mb-7">
             914 MAGAZINE AD
           </h4>
-          <img src={magazine2} alt="Magazine page ad for Castle Hotel and spa" className="p-10" />
+          <img
+            src={magazine2}
+            alt="Magazine page ad for Castle Hotel and spa"
+            className="p-10"
+          />
         </div>
+      </div>
+      <div className="textDivider mb-14 ">
+        <p className="font-span md:text-6xl text-xl text-center">
+          A Castle that doesn't deliver<br></br> the
+          <span className="font-spanlight"> royal treatment</span> at every
+          touch point, <br></br> is no Castle at all.
+        </p>
+      </div>
+      <VideoOverlay
+        videoSrc={webPageWalkThrough}
+        imageSrc={fourthImage}
+        altText="Video Walkthrough of the Castle and Spa Website"
+      />
+
+      <div className="text-center">
+        <h5 className="font-span ml:text-4xl text-2xl mt-5">
+          PROGRAMMING + EVENTS
+        </h5>
+        <p className="font-napzer px-14 my-4">
+          An idyllic destination where innovation meets inspiration. Below are
+          just some <br></br>of the programs we put in place.
+        </p>
+      </div>
+      <div className="imageAndText flex flex-col md:grid md:grid-cols-2">
+        <img
+          src={fifthImage}
+          alt="The Mountain View Grand Llama and Farmer"
+          className="w-full md:w-auto p-8"
+        />
+        <div className="text-center md:text-left md:mt-80 mt-0">
+          <h5 className="font-spanlight text-lg md:text-4xl italic md:my-3">
+            DRAG QUEEN BRUNCH<br></br>AS YOU WISH HAPPY HOUR<br></br>
+            SUMMER FRIDAYS AT THE GROTTO
+          </h5>
+        </div>
+      </div>
+      <div className="m-16">
+        <h5 className="text-center font-span text-2xl">MORE WORK</h5>
       </div>
     </div>
   );
