a52d73d6fd4c4df113a1f64354ad441c73d30bfe
Michael Mount <mount.photos@gmail.com>
2024-02-19 14:02:29 -0700

    built gsap fadeinaniamtion component

---
+++ src/components/fadeInAnimation/index.js
@@ -0,0 +1,39 @@
+import { useRef, useEffect } from "react";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+const FadeInAnimation = ({ text, wrapperClassName = " " }) => {
+  const ref = useRef(null);
+
+  useEffect(() => {
+    gsap.registerPlugin(ScrollTrigger);
+    const element = ref.current;
+
+    gsap.fromTo(
+      element,
+      {
+        autoAlpha: 0,
+      },
+      {
+        duration: 1,
+        autoAlpha: 1,
+        ease: "none",
+        scrollTrigger: {
+          trigger: element,
+          start: "top 80%",
+          end: "bottom 20%",
+          toggleActions: "play none none reverse",
+          once: true,
+        },
+      }
+    );
+  }, []);
+
+  return (
+    <div ref={ref} className={wrapperClassName}>
+      {text}
+    </div>
+  );
+};
+
+export default FadeInAnimation
--- src/components/pages/homepage.js
+++ src/components/pages/homepage.js
@@ -1,10 +1,14 @@
 import FlipInAnimation from "../flipInAnimation";
+import FadeInAnimation from "../fadeInAnimation";
 
 export default function Homepage() {
   return (
     <>
-      <div className="text-center font-span text-9xl m-24">
+      <div className="text-center font-span text-9xl md:mt-24 md:mb-16">
         <FlipInAnimation text="10 and 5" />
+      </div>
+      <div className="text-center font-napzer text-2xl">
+      <FadeInAnimation text="A non-verbal  cue, we’ve made our mantra." />
       </div>
     </>
   );
