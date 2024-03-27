0edac5009679ead7e973469087bc5b2ed1e64eb1
Michael Mount <mount.photos@gmail.com>
2024-03-27 03:31:17 -0600

    adjusted changes for castle subpage

--- src/components/pages/castleSubPage.js
+++ src/components/pages/castleSubPage.js
@@ -1,14 +1,10 @@
 //Images
 import heroImage from "../images/castleHotel/Hero Image.jpg";
-import secondImage from "../images/castleHotel/2nd Photo.png";
-import thridImage from "../images/castleHotel/3rd Photo.jpg";
+import secondImage from "../images/castleHotel/Castle Logo Section.png";
+import thridImage from "../images/castleHotel/Castle Magazine Photo.png";
 import fourthImage from "../images/castleHotel/4th Photo.jpg";
 import fifthImage from "../images/castleHotel/5th Photo.jpg";
 import waxSeal from "../images/castleHotel/Wax Seal.png";
-
-//Magazine Images
-import magazine1 from "../images/castleHotel/Magazine Photo 1.jpg";
-import magazine2 from "../images/castleHotel/Magazine Photo 2.jpg";
 
 //Videos
 import webPageWalkThrough from "../videos/castle/Castle Hotel and Spa- Website Screen Recording.mp4";
@@ -17,14 +13,13 @@ import webPageWalkThrough from "../videos/castle/Castle Hotel and Spa- Website S
 import TextScroll from "../textScroll";
 import VideoOverlay from "../videoOverlay";
 import NavBar from "../navbar";
-import Footer from "../footer"
+import Footer from "../footer";
 
 export default function CastleSubPage() {
   return (
     <div>
-      
       <div className="relative">
-      <NavBar isTransparent={true} colorTheme="black" showLogo={true} />
+        <NavBar isTransparent={true} colorTheme="black" showLogo={true} />
 
         <img
           src={heroImage}
@@ -36,46 +31,28 @@ export default function CastleSubPage() {
         <div>
           <h2 className="font-span text-3xl md:text-5xl">CASTLE HOTEL & SPA</h2>
           <p className="text-xl md:text-3xl font-spanlight italic">
-            A world away from home
+            A World Away from Home
           </p>
           <p className="text-sm md:text-1xl font-napzer mt-5">
             BRANDING / ADVERTISING CAMPAIGNS / STRATEGY / COPYWRITING
           </p>
         </div>
         <div>
-          <p className="font-napzer md:pl-24 text-xs md:text-sm mt-5 md:mt-0">
-            Castle Hotel & Spa is a one-of-a-kind renovated castle oasis perched
-            majestically atop one of the highest points in the region
-            overlooking the historic Hudson River in lower Hudson Valley of
-            Westchester County, New York. We were partnered with Castle Hotel &
-            Spa as their in-house creative agency, with the task of establishing
-            social presence, increasing brand awareness, social engagement and
-            growth.
+          <p className="font-napzer px-10 md:pl-24 text-xs md:text-sm mt-5 md:mt-0">
+            Once home to an iconic wedding venue, special occasion restaurant
+            and award winning spa, The Castle at Tarrytown lay dormant since the
+            worldwide pandemic. Our team helped bring this fortress back to life
+            with a brand refresh, programming strategy as well as an engaging
+            online presence that launched them out of the middle ages and into
+            the 21st century.
           </p>
-          <h3 className="text-xl text-center font-span mt-8">
-            MEASURED SUCCESS
-          </h3>
-          <div className="grid grid-cols-1 md:grid-cols-3 text-center text-xs md:text-sm p-5 gap-y-3 -mt-4">
-            <p className="font-napzer">
-              +<span className="font-span text-lg">17.8k</span> <br></br> Social
-              growth
-            </p>
-            <p className="font-napzer">
-              +<span className="font-span text-lg">63K</span> <br></br> Website
-              Traffic
-            </p>
-            <p className="font-napzer">
-              +<span className="font-span text-lg">13.8k</span> <br></br>{" "}
-              Interactions
-            </p>
-          </div>
         </div>
       </div>
       <div className="textdivder flex justify-center items-center">
         <img
           src={secondImage}
-          alt="The handpainted archways in the castle"
-          className="w-72 p-1 md:p-3 md:w-full mb-6 md:mb-0"
+          alt="breakdown of the custom font built"
+          className="md:p-3 md:w-full mb-6 md:mb-0"
         />
       </div>
       <div className="flex flex-col items-center mx-4 md:m-12 mt-4">
@@ -107,7 +84,10 @@ export default function CastleSubPage() {
           text="THE ROYAL TREATMENT IS OUR GOLD STANDARD"
         />
       </div>
-      <div className="flex flex-col items-center mx-4 md:m-12 -m-12">
+      <div className="magzinePages">
+        <img src={thridImage} alt="magazine" className="mb-12" />
+      </div>
+      <div className="flex flex-col items-center mx-4 md:m-12 ">
         <div>
           <h3 className="text-3xl md:text-8xl md:absolute md:left-64 md:ml-2 md:mt-6 2xl:ml-56 mr-36 mt-6 font-span">
             TIME OFF
@@ -124,28 +104,7 @@ export default function CastleSubPage() {
           </h3>
         </div>
       </div>
-      <div className="magzinePages flex flex-col md:grid md:grid-cols-2 md:mt-36 mt-14">
-        <div>
-          <h4 className="font-napzer text-center -mb-7 md:-mb-7">
-            WESTCHESTER & HUDSON VALLEY MAGAZINE
-          </h4>
-          <img
-            src={magazine1}
-            alt="Magazine page highlting the wedding venue at Castle Hotel and spa"
-            className="p-10"
-          />
-        </div>
-        <div>
-          <h4 className="font-napzer text-center -mb-7 md:-mb-7">
-            914 MAGAZINE AD
-          </h4>
-          <img
-            src={magazine2}
-            alt="Magazine page ad for Castle Hotel and spa"
-            className="p-10"
-          />
-        </div>
-      </div>
+      
       <div className="textDivider mb-14 ">
         <p className="font-span md:text-6xl text-xl text-center">
           A Castle that doesn't deliver<br></br> the
@@ -181,6 +140,21 @@ export default function CastleSubPage() {
           </h5>
         </div>
       </div>
+      <h3 className="text-xl text-center font-span mt-8">MEASURED SUCCESS</h3>
+      <div className="flex flex-col md:flex-row justify-center items-center text-center text-xs md:text-sm p-5 gap-4 md:gap-2 -mt-4">
+        <p className="font-napzer m-0">
+          +<span className="font-span text-lg">+17.8K</span> <br /> Social
+          growth
+        </p>
+        <p className="font-napzer m-0">
+          +<span className="font-span text-lg">+63K</span> <br /> Website
+          Traffic
+        </p>
+        <p className="font-napzer m-0">
+          +<span className="font-span text-lg">+13.8K</span> <br /> Interactions
+        </p>
+      </div>
+
       <div className="m-16">
         <h5 className="text-center font-span md:text-2xl text-lg">MORE WORK</h5>
         <div className="md:grid md:grid-cols-2 md:gap-72 md:mt-12 flex flex-col items-center font-span md:text-8xl text-2xl mt-6 uppercase">
